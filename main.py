9fc4a39e3937a87025ab35ce7bacd669eff587ca
nicolas-fanjas-mercere <91787661+nicolas-fanjas-mercere@users.noreply.github.com>
2023-01-02 12:15:56 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import string
+alphabet = (string.ascii_lowercase)
+alphabetinverse =(string.ascii_lowercase[::-1])
+def ma_string():
+    print("je suis un beau string")
+
+ma_string()
+print(alphabet)
+print(alphabetinverse)
+
+def addition():
+    num1=40
+    num2=2
+    print(num1+num2)
+
+addition()
+
+
+def produit():
+    num1=3
+    num2=6
+    print(num1*num2)
+
+produit()
+
